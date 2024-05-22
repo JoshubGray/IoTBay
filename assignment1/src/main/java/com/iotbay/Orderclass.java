166e2e15d9f2aaaafa1d09e84ca05b26da4c9b05
Jaykin6 <161807581+Jaykin6@users.noreply.github.com>
2024-05-22 12:25:44 +1000

    Create Orderclass.java

---
+++ assignment1/src/main/java/com/iotbay/Orderclass.java
@@ -0,0 +1,48 @@
+package com.iotbay.model;
+
+public class Order {
+    private String name;
+    private String email;
+    private int orderId;
+    private String date;
+
+    public Order(String name, String email, int orderId, String date) {
+        this.name = name;
+        this.email = email;
+        this.orderId = orderId;
+        this.date = date;
+    }
+
+    // method of getter and setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
